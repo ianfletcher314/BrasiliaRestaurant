b4c91c0516d50f9f29c41d39dd8c197c4a258b2c
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-02 14:54:37 -0600

    exported main component with defautl text

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+function Main() {
+    return <div>
+        <h1>This is the Main Section</h1>
+        <pre>
+            t 
+            h 
+            <i>is is wierd</i>
+        </pre>
+    </div>
+}
+export default Main
