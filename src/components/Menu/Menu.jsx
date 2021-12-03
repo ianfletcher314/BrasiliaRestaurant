6bcf3c4ba626579514e7ddee4cb9d4f062391add
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-03 16:08:43 -0600

    added a use state for the menu display and set it to the boolean true

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,9 @@
 // ------------this is where any files this component needs are imported-----------
-
+import React,{useState} from 'react'
 import './style.css'
 // ------------this is the function that returns this component's UI---------------
 function Menu() {
+    const [displayMenu,setDisplayMenu] = useState(true)
     return <div className="menu">
     <h1>menu div</h1>
     </div>;
