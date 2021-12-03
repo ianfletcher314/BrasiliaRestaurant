9e567baab23086d397b0b9d887a95e5682cdf65f
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-03 16:13:46 -0600

    added in display menu state to menu.jsx and now have a div that dynamically renders depending on the display menu state

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,10 +3,19 @@ import React,{useState} from 'react'
 import './style.css'
 // ------------this is the function that returns this component's UI---------------
 function Menu() {
-    const [displayMenu,setDisplayMenu] = useState(true)
-    return <div className="menu">
+    const [displayMenu,setDisplayMenu] = useState(false)
+    if (displayMenu){
+      return <div className="menu">
     <h1>menu div</h1>
+    <button onClick={()=>{setDisplayMenu(false)}}>hide menu</button>
     </div>;
+    }
+    else {
+      return <div>
+         <button onClick={()=>{setDisplayMenu(true)}}>show menu</button>
+      </div>
+    }
+    
   }
 
 export default Menu
