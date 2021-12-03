7ec6cc52d449c7089f27c46f60cfce079a43efc2
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-03 16:02:40 -0600

    added imported usestate into app and created a state and a function to change that state. I also added two buttons tochange the text of a h1 tag depending on the state of the website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [status, setStatus] = useState("open");
+  return (
+    <div className="App">
+      <h1> status : {status}</h1>
+      <button
+        onClick={() => {
+          setStatus("closed");
+        }}
+      >
+        Close Status
+      </button>
+      <button
+        onClick={() => {
+          setStatus("open");
+        }}
+      >
+        Open Status
+      </button>
+    </div>
+  );
 }
 
 export default App;
