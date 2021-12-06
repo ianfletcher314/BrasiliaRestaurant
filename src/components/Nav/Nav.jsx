a70fbdba74dc648412e5342ddee698e8d8fb2662
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-06 12:20:12 -0600

    added dashbaord functionality where the menu button takes away the nav section and only leavs the close menu button

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,21 +1,55 @@
 // ------------this is where any files this component needs are imported-----------
-import React,{useState} from 'react'
-import './style.css'
+import React, { useState } from "react";
+import "./style.css";
 // ------------this is the function that returns this component's UI---------------
+
 function Nav() {
-    return <div >
-      <ul className="nav justify-content-end bg-dark">
+  const [navStatus, setNavStatus] = useState(true);
+  if (navStatus) {
+    return (
+      <div>
+        <ul className="nav justify-content-end bg-dark">
           <li className="nav-item">
-              <a href="#dinner" className="nav-link active btn">Dinner</a>
+            <a href="#dinner" className="nav-link active btn">
+              Dinner
+            </a>
           </li>
           <li className="nav-item">
-              <a href="#lunch" className="nav-link active btn">Lunch</a>
+            <a href="#lunch" className="nav-link active btn">
+              Lunch
+            </a>
           </li>
           <li className="nav-item">
-            <a href="#Menu" className="nav-link active btn">Menu</a>
+            <button
+              onClick={() => {
+                setNavStatus(false);
+              }}
+              className="nav-link active btn"
+            >
+              Show Menu
+            </button>
           </li>
-      </ul>
-    </div>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ul className="nav justify-content-end bg-dark">
+          <li className="nav-item">
+            <button
+              onClick={() => {
+                setNavStatus(true);
+              }}
+              className="nav-link active btn"
+            >
+              Show Menu
+            </button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Nav
+export default Nav;
