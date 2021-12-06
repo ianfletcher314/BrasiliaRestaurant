728d9851bfdc2c324850d39e08056cac01051b55
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-05 18:03:22 -0600

    added in nav section ul with apropriate tags and bootstrap classes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,9 +2,18 @@
 import './style.css'
 // ------------this is the function that returns this component's UI---------------
 function Nav() {
-    return <div className="nav row justify-content-center">
-        <h1>This is the nav section</h1>
-        <p>getting better at this jsx shit</p>
+    return <div >
+      <ul className="nav justify-content-end">
+          <li className="nav-item">
+              <a href="#dinner" className="nav-link active">Dinner</a>
+          </li>
+          <li className="nav-item">
+              <a href="#lunch" className="nav-link active">Lunch</a>
+          </li>
+          <li className="nav-item">
+            <a href="#Menu" className="nav-link active">Menu</a>
+          </li>
+      </ul>
     </div>
 }
 
