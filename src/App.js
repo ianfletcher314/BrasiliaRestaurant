0e913cfd597db99ad94ccd3d7949ac6dce5e11e8
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-07 13:00:29 -0600

    added conditional rendering for the menu and main divs based on the a setState. now i need to pass the state change and status down to the menu button so that button will have the functionality and not a random status button

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import Main from "./components/Main/Main";
 import Menu from "./components/Menu/Menu";
 
 function App() {
-  const [status, setStatus] = useState("open");
+  const [status, setStatus] = useState(true);
   const statusFunction = () => {
-    setStatus("closed");
+    setStatus(!status);
   };
-  return (
-    <div className="App row justify-content-center">
-      <h1> App.js status : {status}</h1>
-      <Nav function={statusFunction} />
-      <Menu />
-      <Main />
-    </div>
-  );
+  if (status === true) {
+    return (
+      <div className="App row justify-content-center">
+        <h1> App.js status : {status}</h1>
+        <Nav function={statusFunction} />
+        <Main />
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <Nav function={statusFunction} />
+        <Menu />
+      </>
+    );
+  }
 }
 
 export default App;
