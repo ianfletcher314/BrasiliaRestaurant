b5bc4da64e1f530cf5d0a95216e56ba11479b4c9
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-08 16:05:09 -0600

    added video component to churrasco to see if I can amke it abackground image

--- src/components/Currasco/Churrasco.jsx
+++ src/components/Currasco/Churrasco.jsx
@@ -1,6 +1,14 @@
 import "./style.css";
 
 function Churrasco() {
-  return <div id="churrasco">churrasco</div>;
+  return (
+    <div id="churrasco">
+      <video width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4" />
+        <source src="movie.ogg" type="video/ogg" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
 export default Churrasco;
