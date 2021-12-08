260f4d24f1962399cd7b68f98224c884938c254c
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-08 14:37:51 -0600

    put Nav component into main div and sucessfully transfered props down to the Nav component so that the nav section still works. there are two nav sections so I will need to take the one out of App.js

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
 // ------------this is where any files this component needs are imported-----------
-
+import Nav from "../Nav/Nav";
 import "./style.css";
 // ------------this is the function that returns this component's UI---------------
-function Main() {
-  return <div className="main row justify-content-center">main div</div>;
+function Main(props) {
+  return (
+    <div className="main row justify-content-center">
+      <Nav function={props.function} btnStatus={props.btnStatus} />
+    </div>
+  );
 }
 export default Main;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 // ------------this is where any files this component needs are imported-----------
 import React, { useState } from "react";
 import "./style.css";
+
 // ------------this is the function that returns this component's UI---------------
 
 function Nav(props) {
