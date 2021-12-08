16c58f4b4dd0a2f3a6cba63dd46b08eb86092e6d
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-08 14:31:59 -0600

    added height to main div to make it fill the first screen

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,8 @@
 // ------------this is where any files this component needs are imported-----------
 
-import './style.css'
+import "./style.css";
 // ------------this is the function that returns this component's UI---------------
 function Main() {
-    return <div className="main row justify-content-center">
-        <h1>This is the Main Section</h1>
-        <pre>
-            t 
-            h 
-            <i>is is wierd</i>
-        </pre>
-    </div>
+  return <div className="main row justify-content-center">main div</div>;
 }
-export default Main
+export default Main;
