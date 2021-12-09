b0d325b1a25b8308afb1ca8985ca3866082846de
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-09 15:38:59 -0600

    got brasilia logo in the middle now I needa black div to go over it and white text color

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,12 @@ import "./style.css";
 import drone from "../../images/drone.mp4";
 function Home() {
   return (
-    <>
+    <div className="video-container">
       <video muted autoPlay loop id="home">
         <source src={drone} type="video/mp4" />
       </video>
-    </>
+      <div className="col-12 textHolder2 logologo">BRASILIA</div>
+    </div>
   );
 }
 export default Home;
