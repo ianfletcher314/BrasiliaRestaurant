f4c9f6cf94a8831606dbcc620020998fbca6aa94
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-09 11:43:28 -0600

    added in tow sections for the phone number area nad the hours area

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,8 +2,13 @@ import "./style.css";
 
 function Contact() {
   return (
-    <div className="contact" id="contact">
-      contact
+    <div className="contact row" id="contact">
+      <div id="phone" className="col-5">
+        phone{" "}
+      </div>
+      <div id="hours" className="col-6">
+        hours
+      </div>
     </div>
   );
 }
