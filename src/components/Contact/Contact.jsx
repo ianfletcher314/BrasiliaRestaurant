f05d63d7d141e981adef2d29a5ea4447b96e1807
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-10 16:38:15 -0600

    added location and hours values to contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,14 @@ function Contact() {
   return (
     <div className="contact row" id="contact">
       <div id="phone" className="col-5">
-        phone{" "}
+        <h2>3212 Grand Blvd</h2>
+        <h3>St. Louis, MO</h3>
+        <ul>
+          <li>Wednesday || Tursday: 5pm - 9pm </li>
+          <li>Friday || Saturday: 5pm - 10pm </li>
+          <li>Sunday Brunch Buffet: 11am - 2:30pm </li>
+          <li>Sunday Dinner: 5pm - 9pm </li>
+        </ul>
       </div>
       <div id="hours" className="col-6">
         hours
