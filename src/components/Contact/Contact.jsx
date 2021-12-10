8de51a7a3bc58b4508d91d680c6988628f3c40d6
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-10 16:41:55 -0600

    changed the formatting of the contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,12 +6,14 @@ function Contact() {
       <div id="phone" className="col-5">
         <h2>3212 Grand Blvd</h2>
         <h3>St. Louis, MO</h3>
-        <ul>
-          <li>Wednesday || Tursday: 5pm - 9pm </li>
-          <li>Friday || Saturday: 5pm - 10pm </li>
-          <li>Sunday Brunch Buffet: 11am - 2:30pm </li>
-          <li>Sunday Dinner: 5pm - 9pm </li>
-        </ul>
+        <div className="hours-holder">
+          <ul>
+            <li>Wednesday || Tursday: 5pm - 9pm </li>
+            <li>Friday || Saturday: 5pm - 10pm </li>
+            <li>Sunday Brunch Buffet: 11am - 2:30pm </li>
+            <li>Sunday Dinner: 5pm - 9pm </li>
+          </ul>
+        </div>
       </div>
       <div id="hours" className="col-6">
         hours
