18c7797a32cba493b5acaeac40a632f4e2f70e04
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-13 14:40:20 -0600

    added link status to the phone number

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,9 @@ function Contact() {
       <div id="phone" className="col-5">
         <div className="hours-holder">
           <h2 className="logoSmall">Reserve a table now!</h2>
-          <h3 className="logoSmall">314-932-1034</h3>
+          <h3 className="logoSmall">
+            <a href="314-932-1034">314-932-1034</a>
+          </h3>
           <ul>
             <li>Wednesday || Tursday: 5pm - 9pm </li>
             <li>Friday || Saturday: 5pm - 10pm </li>
@@ -18,7 +20,6 @@ function Contact() {
       <div id="hours" className="col-6">
         <div className="map-holder">
           <h3 className="logoSmall roof">3212 Grand Bulivard St. Louis, MO</h3>
-          {/* <h4></h4> */}
           <iframe
             title="googleMap"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3118.1865676419293!2d-90.24492294883655!3d38.59857587200708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d8b4683ad31d91%3A0x7a790fceb95709e3!2sBrasilia%20Bar%20%26%20Restaurant!5e0!3m2!1sen!2sus!4v1639177245498!5m2!1sen!2sus"
