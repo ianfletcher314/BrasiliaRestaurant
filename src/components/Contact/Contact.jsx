9085491184872d94db7d47d70164b2011d409f46
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-13 14:19:05 -0600

    resized map item in contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -18,9 +18,10 @@ function Contact() {
       <div id="hours" className="col-6">
         <div className="map-holder">
           <iframe
+            title="googleMap"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3118.1865676419293!2d-90.24492294883655!3d38.59857587200708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d8b4683ad31d91%3A0x7a790fceb95709e3!2sBrasilia%20Bar%20%26%20Restaurant!5e0!3m2!1sen!2sus!4v1639177245498!5m2!1sen!2sus"
-            width="600"
-            height="300"
+            width="550"
+            height="250"
             allowfullscreen=""
             loading="lazy"
             id="googleMap"
