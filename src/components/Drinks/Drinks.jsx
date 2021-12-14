53dfa268b1a915f04033bae179a100ee8b6b54f7
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-13 19:43:59 -0600

    added link to drink section with hovering icons

--- src/components/Drinks/Drinks.jsx
+++ src/components/Drinks/Drinks.jsx
@@ -3,13 +3,17 @@ function Drinks() {
   return (
     <div id="drinks" className="row align-items-center">
       <div className="col-8 offset-md-2 textHolder row">
-        <div className="col-6 drinkText">
+        <div className="col-md-6 drinkText">
           Our hand-crafted coctails bring a modern flare to tradional Brasilian
           standards. The Caipirinha is the National Drink of Brasil and uses a
           sugar cane brandy called Cachaça. Make it a double to get the night
           started right!
         </div>
-        <div className="col-6 drinkBtn btn">Drink Menu</div>
+        <div className="col-md-5 offset-md-1 drinkLink row align-items-center">
+          <button className="btn drinkBtn text-light logoSmall">
+            Drink Menu
+          </button>
+        </div>
       </div>
     </div>
   );
