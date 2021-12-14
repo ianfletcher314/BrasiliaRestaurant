4492a92c87c6ba7a5ef91a2e2dff250531dd0429
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-13 19:13:27 -0600

    wrote text for drinks div

--- src/components/Drinks/Drinks.jsx
+++ src/components/Drinks/Drinks.jsx
@@ -2,7 +2,15 @@ import "./style.css";
 function Drinks() {
   return (
     <div id="drinks" className="row align-items-center">
-      <div className="col-8 offset-md-2 textHolder align-items-center"></div>
+      <div className="col-8 offset-md-2 textHolder row">
+        <div className="col-6 drinkText">
+          Our hand-crafted coctails bring a modern flare to tradional Brasilian
+          standards. The Caipirinha is the National Drink of Brasil and uses a
+          sugar cane brandy called Cachaça. Make it a double to get the night
+          started right!
+        </div>
+        <div className="col-6 drinkBtn btn">Drink Menu</div>
+      </div>
     </div>
   );
 }
