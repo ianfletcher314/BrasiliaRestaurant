a1e007945b458d7b3249bdd30129227d67d0ab54
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-13 18:58:12 -0600

    added in grey see through div to drinks

--- src/components/Drinks/Drinks.jsx
+++ src/components/Drinks/Drinks.jsx
@@ -1,5 +1,9 @@
 import "./style.css";
 function Drinks() {
-  return <div id="drinks"></div>;
+  return (
+    <div id="drinks" className="row align-items-center">
+      <div className="col-8 offset-md-2 textHolder align-items-center"></div>
+    </div>
+  );
 }
 export default Drinks;
