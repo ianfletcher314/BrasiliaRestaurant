55f5f6858e4850b683c25bdcc163e9767bb662f7
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-13 18:13:55 -0600

    moved description of churrasco to one sside and now I need to make a new div that holds a video and description

--- src/components/Currasco/Churrasco.jsx
+++ src/components/Currasco/Churrasco.jsx
@@ -5,7 +5,7 @@ function Churrasco() {
     <>
       <div id="churrasco" className="row align-items-center">
         <div className="col-8 offset-md-2 textHolder row">
-          <div className="text-justify churrascoSmall">
+          <div className=" col-6 text-justify churrascoSmall">
             In Brazil, churrasco is the term for a barbecue which originated in
             southern Brazil. It uses a variety of meats, pork, sausage and
             chicken which may be cooked on a purpose-built "churrasqueira", a
