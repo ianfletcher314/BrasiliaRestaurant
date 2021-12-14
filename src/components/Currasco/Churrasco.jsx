412c3bfeb2574e6538748c7723921734cc2a3b4f
Ian Fletcher <ianfletcher314@gmail.com>
2021-12-13 18:03:41 -0600

    added new font and changed margins for the churrasco div

--- src/components/Currasco/Churrasco.jsx
+++ src/components/Currasco/Churrasco.jsx
@@ -4,8 +4,14 @@ function Churrasco() {
   return (
     <>
       <div id="churrasco" className="row align-items-center">
-        <div className="col-8 offset-md-2 textHolder">
-          THIS IS WHREE THE TEXT GOES
+        <div className="col-8 offset-md-2 textHolder row">
+          <div className="text-justify logoSmall">
+            In Brazil, churrasco is the term for a barbecue which originated in
+            southern Brazil. It uses a variety of meats, pork, sausage and
+            chicken which may be cooked on a purpose-built "churrasqueira", a
+            barbecue grill with supports for spits or skewers. Come taste the
+            tradition every friday night
+          </div>
         </div>
       </div>
     </>
