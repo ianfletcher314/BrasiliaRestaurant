e0f54081a3b474a89e4db0c7815b73c5e9512c3e
Ian Fletcher <ianfletcher314@gmail.com>
2022-01-04 15:09:29 -0600

    added menu to menu component now need to resize

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 import "./style.css";
 // ------------this is the function that returns this component's UI---------------
 function Menu() {
-  return <div className="box"></div>;
+  return (
+    <div className="box">
+      <div className="container dinnerMenu"></div>
+    </div>
+  );
 }
 
 export default Menu;
