e84d1f3fd536a15d456b287087cfce2601b32773
Ian Fletcher <ianfletcher314@gmail.com>
2022-01-04 15:02:44 -0600

    changed background color to menu page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,21 +1,9 @@
 // ------------this is where any files this component needs are imported-----------
-import React,{useState} from 'react'
-import './style.css'
+import React, { useState } from "react";
+import "./style.css";
 // ------------this is the function that returns this component's UI---------------
 function Menu() {
-    const [displayMenu,setDisplayMenu] = useState(false)
-    if (displayMenu){
-      return <div className="menu row justify-content-center">
-    <h1>menu div</h1>
-    <button className="btn btn-outline-primary col-3" onClick={()=>{setDisplayMenu(false)}}>hide menu</button>
-    </div>;
-    }
-    else {
-      return <div className="row justify-content-center" >
-         <button className="col-3 btn btn-outline-primary" onClick={()=>{setDisplayMenu(true)}}>show menu</button>
-      </div>
-    }
-    
-  }
+  return <div className="box"></div>;
+}
 
-export default Menu
+export default Menu;
