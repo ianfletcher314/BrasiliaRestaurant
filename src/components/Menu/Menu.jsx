2d7d3c8fb69c544eb6a656e0ebd7f3b50a85b1dd
Ian Fletcher <ianfletcher314@gmail.com>
2022-01-04 15:25:51 -0600

    imported drinks menu photo to menu.jsx and used it in an image tag below the tag holding the dinner menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,19 @@
 // ------------this is where any files this component needs are imported-----------
 import "./style.css";
 import DinnerMenu from "../../images/DinnerMenu.jpg";
+import DrinkMenu from "../../images/DrinkMenu.jpg";
 // ------------this is the function that returns this component's UI---------------
 function Menu() {
   return (
     <div className="box row ">
       <img
-        class="col-8 offset-2  dinnerMenu"
+        class="col-8 offset-2  menuHolder"
         src={DinnerMenu}
+        alt="Dinner Menu"
+      />
+      <img
+        class="col-8 offset-2  menuHolder"
+        src={DrinkMenu}
         alt="Dinner Menu"
       />
     </div>
