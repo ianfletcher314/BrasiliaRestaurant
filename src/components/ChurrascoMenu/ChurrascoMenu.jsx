35db1b861cf8e3f6f18a0cafe5fe190e100670c3
Ian Fletcher <ianfletcher314@gmail.com>
2022-01-06 13:39:50 -0600

    added export default to drink menu and churrasco menu

--- src/components/ChurrascoMenu/ChurrascoMenu.jsx
+++ src/components/ChurrascoMenu/ChurrascoMenu.jsx
@@ -1,1 +1,5 @@
 import "./style.css";
+function ChurrascoMenu() {
+  return <h1>churrasco menu</h1>;
+}
+export default ChurrascoMenu;
--- src/components/DrinkMenu/DrinkMenu.jsx
+++ src/components/DrinkMenu/DrinkMenu.jsx
@@ -1,1 +1,5 @@
 import "./style.css";
+function DrinkMenu() {
+  return <h1>drink menu</h1>;
+}
+export default DrinkMenu;
