af15d8bb9fdc081113660d69ee2b19d03a687444
Ian Fletcher <ianfletcher314@gmail.com>
2022-01-10 11:41:04 -0600

    added comments to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// --------------------------this is where any files are imported---------------------------
 import React, { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
@@ -5,7 +6,7 @@ import Main from "./components/Main/Main";
 import Menu from "./components/Menu/Menu";
 import DrinkMenu from "./components/DrinkMenu/DrinkMenu";
 import ChurrascoMenu from "./components/ChurrascoMenu/ChurrascoMenu";
-
+// ----------------------this is the function that returns this component's UI-------------------
 function App() {
   const stateObject = {
     page: "home",
@@ -15,12 +16,14 @@ function App() {
     setStatus({ page: newState });
   };
   var btnStatus = status;
+  // ---------------------home state conditional rendering of components------------------
   if (status.page === "home") {
     return (
       <div className="App row justify-content-center">
         <Main function={statusFunction} btnStatus={btnStatus} />
       </div>
     );
+    // ----------------------menu state conditional rendering of components-----------------
   } else if (status.page === "menu") {
     return (
       <>
@@ -28,6 +31,7 @@ function App() {
         <Menu />
       </>
     );
+    // ----------------------drink state conditional rendering of components-----------------
   } else if (status.page === "drink") {
     return (
       <>
@@ -35,6 +39,7 @@ function App() {
         <DrinkMenu />
       </>
     );
+    // --------------------churrasco state conditional rendering of components---------------
   } else if (status.page === "churrasco") {
     return (
       <>
